b8a22a0072ac2e6ed2a6090766c1e31bfe6e1730
Andrew Bolton <andrewbolton@Andrews-MacBook-Pro.local>
2022-02-04 04:05:21 +0000

    removed unused code and tidied imports

--- index.js
+++ index.js
@@ -1,37 +1,13 @@
 require("dotenv").config();
-const DiscordJS = require("discord.js");
+const { Client, Intents } = require("discord.js");
 
-const client = new DiscordJS.Client({
-  intents: [
-    DiscordJS.Intents.FLAGS.GUILDS,
-    DiscordJS.Intents.FLAGS.GUILD_MESSAGES,
-  ],
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.on("ready", () => {
   console.log("The bot is ready");
 });
-
-// client.on("messageCreate", (msg) => {
-//   const newArr = msg.mentions.roles.map((item) => {
-//     return item;
-//   });
-//   let instructorBool = false;
-//   for (let i = 0; i < newArr.length; i++) {
-//     if (
-//       newArr[i].name === "Innovation Developer" ||
-//       newArr[i].name === "Junior Developer"
-//     ) {
-//       instructorBool = true;
-//     }
-//   }
-//   if (instructorBool === true) {
-//     msg.reply({
-//       content:
-//         "Please answer the following questions to help us identify a solution: \n What topic/project/challenge I need help with? \n What I expect my code to do? \n What is actually happening? \n What I've tried to do to fix it?",
-//     });
-//   }
-// });
 
 client.on("messageCreate", (msg) => {
   let bool = false;
